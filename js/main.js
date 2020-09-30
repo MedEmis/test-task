f23b7b20d073f641cc5132aae51daa57b49fa795
MedEmis <67052751+MedEmis@users.noreply.github.com>
2020-09-30 15:21:53 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,1 @@
+let menuBurger=document.querySelector(".header-menu__icon"),sideMenu=document.querySelector(".header__menu");menuBurger.addEventListener("click",(function(e){menuBurger.classList.toggle("active"),sideMenu.classList.toggle("active")}));
